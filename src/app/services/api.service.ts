b72ece585686a82a57e4fba50f0f04970859be63
Georgiy Dzesov <g.dzesov@gmail.com>
2018-11-01 13:32:44 +0300

    add auth interceptor

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class ApiService {
-  private baseUrl = 'https://backend.acme-company.site';
+  private baseUrl = '/api';
 
   public get login() {
     return `${this.baseUrl}/account/login`;
