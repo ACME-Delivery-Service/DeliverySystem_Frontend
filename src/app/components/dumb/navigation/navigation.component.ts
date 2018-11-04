ecec1ac057f96a661191ad5f1ad72e57d8fdef7c
nicholas <bul-nick-al@ya.ru>
2018-11-04 09:06:08 +0300

    add logout functionality. The logout button can be accessed from the menu called by clicking on the driver card in the navbar

--- src/app/components/dumb/navigation/navigation.component.ts
+++ src/app/components/dumb/navigation/navigation.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
@@ -6,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navigation.component.css']
 })
 export class NavigationComponent implements OnInit {
-  constructor() {}
+  constructor(private authService: AuthService, private router: Router) {}
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['login']);
+  }
 
   ngOnInit() {}
 }
