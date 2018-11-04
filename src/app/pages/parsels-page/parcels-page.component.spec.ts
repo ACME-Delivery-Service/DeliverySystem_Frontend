e2ab42f19004fc0ce32005ff6c78bc4464e1f7db
nicholas <bul-nick-al@ya.ru>
2018-11-04 11:51:40 +0300

    add ability to log in by pressing the Enter key (also tests are back :) )

--- src/app/pages/parsels-page/parcels-page.component.spec.ts
+++ src/app/pages/parsels-page/parcels-page.component.spec.ts
@@ -1,24 +1,24 @@
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-//
-// import { ParselsPageComponent } from './parsels-page.component';
-//
-// describe('ParselsListComponent', () => {
-//   let component: ParselsPageComponent;
-//   let fixture: ComponentFixture<ParselsPageComponent>;
-//
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ParselsPageComponent]
-//     }).compileComponents();
-//   }));
-//
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(ParselsPageComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
-//
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ParcelsPageComponent } from './parsels-page.component';
+
+describe('ParselsListComponent', () => {
+  let component: ParcelsPageComponent;
+  let fixture: ComponentFixture<ParcelsPageComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ParcelsPageComponent]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ParcelsPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
