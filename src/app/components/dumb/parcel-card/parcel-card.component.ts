ebc2a8edacba16a10273853ca5496cfe9a83f34a
nicholas <bul-nick-al@ya.ru>
2018-11-15 12:28:15 +0300

    add dialer (can't call though)

--- src/app/components/dumb/parcel-card/parcel-card.component.ts
+++ src/app/components/dumb/parcel-card/parcel-card.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ParcelInterface } from '../../../interfaces/parcel-interface';
+import { OperatorInterface } from '../../../interfaces/operator-interface';
 
 @Component({
   selector: 'app-parcel-card',
@@ -11,6 +12,22 @@ export class ParcelCardComponent {
   @Input()
   parcel: ParcelInterface;
 
+  delivery_operator: OperatorInterface = {
+    avatar: 'assets/img/avatar.jpg',
+    status: 'Occupied',
+    contacts: {
+      address: null,
+      phone_number: '+7912492904',
+      additional_info: null,
+      first_name: 'Mikhail',
+      last_name: 'Zadin',
+      position: null,
+      company: null
+    },
+    location: null,
+    location_update_time: null,
+    id: 123
+  };
   closeResult: string;
   modalReference: any;
   lat = 55.752134;
@@ -33,5 +50,4 @@ export class ParcelCardComponent {
   close() {
     this.modalReference.close();
   }
-
 }
